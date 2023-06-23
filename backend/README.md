bc9da10189c005fdf0e3a171081284ab73be6406
ytison <yannick.tison@gmail.com>
2023-06-24 01:19:30 +0200

    fix divers

--- backend/README.md
+++ backend/README.md
@@ -1,1 +1,8 @@
 # Description de la partie Back
+
+npx hardhat test
+REPORT_GAS=true npx hardhat test
+
+pour deployer : 
+1 - npx hardhat node
+2 - npx hardhat run scripts/deploy.js --network localhost
---
+++ frontend/context/EthContext.tsx
@@ -0,0 +1,125 @@
+'use client'
+import React, { createContext, useEffect, useState } from "react";
+import {ethers} from "ethers";
+import votingABI from "@/utils/abi";
+import { redirect } from 'next/navigation'
+
+
+//TODO passer en variabel envt
+const votingContractAddress='0x5FbDB2315678afecb367f032d93F642f64180aa3';
+                           
+
+// const VotingContract = new ethers.Contract(votingContractAddress, votingABI, provider);
+
+
+
+const { ethereum } = (typeof window !== "undefined" ? window : {}) as { ethereum: any };
+export const EthContext = createContext(null);
+
+export const EthProvider = ({ children }) => {
+  const [account, setAccount] = useState("");
+  const [error, setError] = useState("");
+  const [chainId, setChainId] = useState("")
+  const [isOwner, setIsOwner] = useState<boolean>(false);
+  
+
+  const checkEthereumExists = () => {
+    if (!ethereum) { 
+      setError("Please Install MetaMask.");
+      return false;
+    }
+    return true;
+  };
+
+  const getConnectedAccounts = async () => {
+    setError("");
+    try {
+      const accounts = await ethereum.request({
+        method: "eth_accounts",
+      });
+      console.log(accounts);
+      setAccount(accounts[0]);
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
+  const connectWallet = async () => {
+    setError("");
+    if (checkEthereumExists()) {
+      try {
+        const accounts = await ethereum.request({ method: "eth_requestAccounts",});
+        setAccount(accounts[0]);
+        console.log(accounts);
+
+
+      } catch (err) {
+        setError(err.message);
+      }
+    }
+  };
+
+
+  useEffect(() => {
+    if (checkEthereumExists()) {
+      ethereum.on("accountsChanged", getConnectedAccounts);
+      getConnectedAccounts();
+      ethereum.on('chainChanged', (chainId) => setChainId(chainId));
+      ethereum.request({ method: "eth_chainId" }).then((chainId) => {
+        setChainId(chainId)
+      });
+    }
+    return () => {
+      if (checkEthereumExists()) {
+        ethereum.removeListener("accountsChanged", getConnectedAccounts);
+      }
+      if (checkEthereumExists()) {
+        ethereum.removeListener("chainChanged", (chainId) => setChainId(chainId));
+      }
+    };
+  }, []);
+
+  //reload page on chain change
+  useEffect(() => {
+  
+  }, []);
+
+  const ethereumWindow = (window as unknown as any).ethereum as import('ethers').providers.ExternalProvider;
+  const provider = new ethers.providers.Web3Provider(ethereumWindow)
+  const contract = new ethers.Contract(votingContractAddress, votingABI, provider)
+
+  
+  
+  useEffect(() => {
+    const getOwner = async () => {
+      const ownerAddress = await contract.owner()
+      return ownerAddress
+    }
+
+    if(account){
+      getOwner().then((ownerAddress) => {
+        console.log(ownerAddress, 'ownerAddress')
+        if(ethers.utils.getAddress(ownerAddress) === ethers.utils.getAddress(account)){
+          setIsOwner(true)
+        } else {
+          setIsOwner(false)
+        }
+      })
+    }
+  }, [account])
+
+  useEffect(() => {
+    console.log(window.location.href)
+    if(isOwner && window.location.href !== "http://localhost:3000/admin"){
+      redirect('/admin')
+    }
+  }, [isOwner])
+
+  console.log({isOwner, account})
+  
+  return (
+    <EthContext.Provider value={{ account, connectWallet, chainId, error }}>
+      {children}
+    </EthContext.Provider>
+  );
+};
