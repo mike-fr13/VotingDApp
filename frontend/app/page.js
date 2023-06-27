bf27985c8702fa79a368485754257cff91b2d493
ytison <yannick.tison@gmail.com>
2023-06-27 18:50:56 +0200

    add Head on mail page

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,12 +1,17 @@
 'use client'
 import React from 'react';
 import Header from '@/components/Header';
+import Head from 'next/head';
 import Main from '@/components/Main';
 import Footer from '@/components/Footer';
 import { Box,Card,  Center,Container } from "@chakra-ui/react";
 
 export default function Home() {
   return (
+    <>
+      <Head>
+        <title>Voting DApp project</title>
+      </Head>
       <Card minHeight="100vh" w='80%' p='5' display="flex" flexDirection="column">
         <Header/>
         <Center>
@@ -16,5 +21,6 @@ export default function Home() {
         </Center>
         <Footer />
       </Card>
+      </>
   );
 }
