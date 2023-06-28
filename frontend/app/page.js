a6bd67a7dc23f310b4b3f6c16763634ad517cbb3
ytison <yannick.tison@gmail.com>
2023-06-28 20:27:49 +0200

    delete Main component

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -2,8 +2,8 @@
 import React from 'react';
 import Header from '@/components/Header';
 import Head from 'next/head';
-import Main from '@/components/Main';
 import Footer from '@/components/Footer';
+import ProposalList from '@/components/ProposalList';
 import { Box,Card,  Center,Container } from "@chakra-ui/react";
 
 export default function Home() {
@@ -12,11 +12,11 @@ export default function Home() {
       <Head>
         <title>Voting DApp project</title>
       </Head>
-      <Card minHeight="100vh" w='80%' p='5' display="flex" flexDirection="column">
+      <Card minHeight="100vh" w='90%' p='5' display="flex" flexDirection="column">
         <Header/>
         <Center>
-          <Container maxW='100%' centerContent>
-            <Main />
+          <Container w='100%' centerContent>
+            <ProposalList/>
           </Container>
         </Center>
         <Footer />
