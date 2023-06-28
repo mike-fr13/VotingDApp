48fbc6b5d79ac1f3aeae27b56652420806f1cc73
ytison <yannick.tison@gmail.com>
2023-06-29 00:25:15 +0200

    fix merge error

--- backend/scripts/inject_data.js
+++ backend/scripts/inject_data.js
@@ -10,15 +10,8 @@ async function main() {
   const loremIpsum =
     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 
-  console.log("addVoter  - account2");
-  await Contract.addVoter(account2);
-  console.log("addVoter  - account3");
-  await Contract.addVoter(account3);
-  console.log("addVoter  - account4");
-  await Contract.addVoter(account4);
-  console.log("addVoter  - account5");
-  await Contract.addVoter(account5);
-
+  const votingABI = require('./abi.js');
+  const { ethers } = require("hardhat");
   const provider = ethers.provider;
   const signer = provider.getSigner();
   const Contract = new ethers.Contract(contractAddress, votingABI, signer);
