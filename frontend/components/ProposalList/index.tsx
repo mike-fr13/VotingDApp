98952b142c94c0d65ae4413536b09face2303dc7
Noam-m33 <noam33700@hotmail.fr>
2023-06-28 21:05:51 +0200

    fix: next js build with window undefined error

--- frontend/components/ProposalList/index.tsx
+++ frontend/components/ProposalList/index.tsx
@@ -1,166 +1,182 @@
-'use client'
-import React from 'react'
-import { useState,useContext,useEffect } from 'react'
-import { EthContext } from '@/context/EthContext'
+"use client";
+import React from "react";
+import { useState, useContext, useEffect } from "react";
+import { EthContext } from "@/context/EthContext";
 import {
-    Stack,StackDivider,
-    Box,
-    Badge,
-    Card,CardHeader,CardBody,
-    Heading,
-    Textarea,
-    Button,
-    Text,
-    useToast
-  } from '@chakra-ui/react'
-import {getProposals} from '@/utils/proposal'
-import { BigNumber } from 'ethers'
-import { Proposal } from '@/types/Proposal'
+  Stack,
+  StackDivider,
+  Box,
+  Badge,
+  Card,
+  CardHeader,
+  CardBody,
+  Heading,
+  Textarea,
+  Button,
+  Text,
+  useToast,
+} from "@chakra-ui/react";
+import { getProposals } from "@/utils/proposal";
+import { BigNumber } from "ethers";
+import { Proposal } from "@/types/Proposal";
 
- export const ProposalList = () => {
-    const { account, isVoter, contractWithSigner } = useContext(EthContext);
-    const [proposals, setProposals] = useState<Proposal[]>([]);
-    const [proposalInputValue, setProposalInputValue] = useState("");
-    const [isSubmittingProposal, setIsSubmittingProposal] = useState(false);
-    const toast = useToast();
-    
-    useEffect(() => {
-      if (isVoter) {
-        const fetchProposals = async () => {
-          if (account && account.length !== 0) {
-            const proposals = await getProposals(account, contractWithSigner);
-            setProposals(proposals);
-          }
-        };
-        fetchProposals();
-      }
-    }, [account, isVoter]);
+export const ProposalList = () => {
+  const { account, isVoter, contractWithSigner } = useContext(EthContext);
+  const [proposals, setProposals] = useState<Proposal[]>([]);
+  const [proposalInputValue, setProposalInputValue] = useState("");
+  const [isSubmittingProposal, setIsSubmittingProposal] = useState(false);
+  const toast = useToast();
 
-    useEffect(() => {
-      if (isVoter) {
-        contractWithSigner.on("ProposalRegistered", (proposalId) => {
-          console.log('ProposalRegistered event : ', proposalId);
-
-          const newProp = contractWithSigner
-            .getOneProposal(proposalId)
-            .then((proposal) => {
-              const newProp: Proposal = {
-                proposalId: proposalId,
-                proposalDescription: proposal.description,
-                nbVote: proposal.voteCount,
-              }
-              setProposals((previousState) => [...previousState, newProp])
-
-            }) 
-          });
-          contractWithSigner.on("Voted", (voterAddress) => {
-            console.log(getProposals(account, contractWithSigner), "helloooooooooooo")
-            toast({ title: 'Vote registered successfully' , status: 'success'})
-            const proposals = getProposals(account, contractWithSigner)
-              .then ((allProposals) => { setProposals(allProposals);});
-          });
-      }
-      return () => {
-        contractWithSigner.removeAllListeners("ProposalRegistered");
-        contractWithSigner.removeAllListeners("Voted");
+  useEffect(() => {
+    if (isVoter) {
+      const fetchProposals = async () => {
+        if (account && account.length !== 0) {
+          const proposals = await getProposals(account, contractWithSigner);
+          setProposals(proposals);
+        }
       };
-    }, []);
-
-
-    const voteForProposal = async (proposalId: BigNumber) => {
-      if (isVoter) {
-        console.log('voteForProposal: ', proposalId);
-        contractWithSigner.setVote(proposalId)
-      }
+      fetchProposals();
     }
+  }, [account, isVoter]);
 
-    async function handleAddProposal() {
-      if (isVoter) {
-        console.log(proposalInputValue);
-        try {
-          const tx = await contractWithSigner.addProposal(proposalInputValue);
-          setIsSubmittingProposal(true);
-          const receipt = await tx.wait();
-          if (receipt.status === 1) {
-            toast({
-              title: `Proposal ${proposalInputValue} registered successfully`,
-              status: "success",
-            });
-          } else {
-            toast({
-              title: "Error",
-              description: "Someting went wrong: Proposal not registered",
-              status: "error",
-            });
+  useEffect(() => {
+    if (isVoter) {
+      contractWithSigner.on("ProposalRegistered", (proposalId) => {
+        console.log("ProposalRegistered event : ", proposalId);
+
+        const newProp = contractWithSigner
+          .getOneProposal(proposalId)
+          .then((proposal) => {
+            const newProp: Proposal = {
+              proposalId: proposalId,
+              proposalDescription: proposal.description,
+              nbVote: proposal.voteCount,
+            };
+            setProposals((previousState) => [...previousState, newProp]);
+          });
+      });
+      contractWithSigner.on("Voted", (voterAddress) => {
+        console.log(
+          getProposals(account, contractWithSigner),
+          "helloooooooooooo"
+        );
+        toast({ title: "Vote registered successfully", status: "success" });
+        const proposals = getProposals(account, contractWithSigner).then(
+          (allProposals) => {
+            setProposals(allProposals);
           }
-          setProposalInputValue("");
-          setIsSubmittingProposal(false);
-        } catch (error) {
-          console.error(error.message);
+        );
+      });
+    }
+    return () => {
+      contractWithSigner.removeAllListeners("ProposalRegistered");
+      contractWithSigner.removeAllListeners("Voted");
+    };
+  }, []);
+
+  const voteForProposal = async (proposalId: BigNumber) => {
+    if (isVoter) {
+      console.log("voteForProposal: ", proposalId);
+      contractWithSigner.setVote(proposalId);
+    }
+  };
+
+  async function handleAddProposal() {
+    if (isVoter) {
+      console.log(proposalInputValue);
+      try {
+        const tx = await contractWithSigner.addProposal(proposalInputValue);
+        setIsSubmittingProposal(true);
+        const receipt = await tx.wait();
+        if (receipt.status === 1) {
+          toast({
+            title: `Proposal ${proposalInputValue} registered successfully`,
+            status: "success",
+          });
+        } else {
           toast({
             title: "Error",
-            description: error.message.slice(0, 500) + "...",
+            description: "Someting went wrong: Proposal not registered",
             status: "error",
           });
         }
+        setProposalInputValue("");
+        setIsSubmittingProposal(false);
+      } catch (error) {
+        console.error(error.message);
+        toast({
+          title: "Error",
+          description: error.message.slice(0, 500) + "...",
+          status: "error",
+        });
       }
     }
+  }
 
-    return (
-    (isVoter)? (
-      <Box p='5' boxSize="4xl">
-          <Heading size='xl'>Proposals Registration</Heading>
-          <Box p='5' w='100%'>
-              <Textarea 
-                  placeholder='Here is your proposal description' 
-                  value={proposalInputValue}
-                  onChange={(e) => setProposalInputValue(e.target.value)}/>
-                  <Button colorScheme='teal' variant='outline' 
-                      isLoading={isSubmittingProposal}
-                      onClick={handleAddProposal}>
-                      Add a proposal
-                  </Button>
-          </Box>
-          <Box w='100%'>
-              {proposals && proposals.length === 0 ? (
-                  <Text fontSize='xl'>There is no proposal yet</Text>
-              ) : (
-                  <Card w='100%'>
-                  <CardHeader>
-                      <Heading size='md'>Proposals List</Heading>
-                  </CardHeader>
-                  {proposals.map((proposal, index) => (
-                  <CardBody key={index}>
-                      <Stack divider={<StackDivider />} spacing='4'>
-                      <Box p='2' border='1px' borderColor='gray.200' w='100%'>
-                          <Heading size='xs' textTransform='uppercase'>
-                          Proposition n° {proposal.proposalId.toString()}
-                          </Heading>
-                          <Box display="flex" justifyContent='space-between'>
-                          <Text w='80%' pt='2' fontSize='sm' id='propDescr'>
-                              {proposal.proposalDescription}
-                          </Text>
-                          <Badge ml='1' fontSize='0.8em' colorScheme='green'>
-                              {proposal.nbVote.toString()}{(proposal.nbVote && proposal.nbVote > BigNumber.from(1)) ? (' votes') : (' vote')}
-                          </Badge>
-                          <Button colorScheme='teal' variant='outline' onClick={() => voteForProposal(proposal.proposalId)}>
-                              Vote
-                          </Button>
-                          </Box>
-                      </Box>
-                      </Stack>
-                  </CardBody>
-                  ))}
-                  </Card>
-              )}
-          </Box>
-      </Box>    
-    ) : (
-      <Box p='5'>
-        <Heading size='xl'>You 're not a registred Voter</Heading>
+  return isVoter ? (
+    <Box p="5" boxSize="4xl">
+      <Heading size="xl">Proposals Registration</Heading>
+      <Box p="5" w="100%">
+        <Textarea
+          placeholder="Here is your proposal description"
+          value={proposalInputValue}
+          onChange={(e) => setProposalInputValue(e.target.value)}
+        />
+        <Button
+          colorScheme="teal"
+          variant="outline"
+          isLoading={isSubmittingProposal}
+          onClick={handleAddProposal}
+        >
+          Add a proposal
+        </Button>
       </Box>
-    )
-  )
-}
+      <Box w="100%">
+        {proposals && proposals.length === 0 ? (
+          <Text fontSize="xl">There is no proposal yet</Text>
+        ) : (
+          <Card w="100%">
+            <CardHeader>
+              <Heading size="md">Proposals List</Heading>
+            </CardHeader>
+            {proposals.map((proposal, index) => (
+              <CardBody key={index}>
+                <Stack divider={<StackDivider />} spacing="4">
+                  <Box p="2" border="1px" borderColor="gray.200" w="100%">
+                    <Heading size="xs" textTransform="uppercase">
+                      Proposition n° {proposal.proposalId.toString()}
+                    </Heading>
+                    <Box display="flex" justifyContent="space-between">
+                      <Text w="80%" pt="2" fontSize="sm" id="propDescr">
+                        {proposal.proposalDescription}
+                      </Text>
+                      <Badge ml="1" fontSize="0.8em" colorScheme="green">
+                        {proposal.nbVote.toString()}
+                        {proposal.nbVote && proposal.nbVote > BigNumber.from(1)
+                          ? " votes"
+                          : " vote"}
+                      </Badge>
+                      <Button
+                        colorScheme="teal"
+                        variant="outline"
+                        onClick={() => voteForProposal(proposal.proposalId)}
+                      >
+                        Vote
+                      </Button>
+                    </Box>
+                  </Box>
+                </Stack>
+              </CardBody>
+            ))}
+          </Card>
+        )}
+      </Box>
+    </Box>
+  ) : (
+    <Box p="5">
+      <Heading size="xl">You're not a registred Voter</Heading>
+    </Box>
+  );
+};
 
-export default ProposalList
+export default ProposalList;
--- frontend/context/EthContext.tsx
+++ frontend/context/EthContext.tsx
@@ -5,33 +5,17 @@ import votingABI from "@/utils/abi";
 import { redirect } from "next/navigation";
 import { Voting } from "@/types/ethers-contracts";
 import { useToast } from "@chakra-ui/react";
-import { Voter } from '@/types/Voter'
-
+import { Voter } from "@/types/Voter";
 
 //TODO passer en variabel envt
 const votingContractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-
-const { ethereum } = (typeof window !== "undefined" ? window : {}) as {
-  ethereum: any;
-};
-const ethereumWindow = (window as unknown as any)
-  .ethereum as import("ethers").providers.ExternalProvider;
-const provider = new ethers.providers.Web3Provider(ethereumWindow);
-
-const contract = new ethers.Contract(
-  votingContractAddress,
-  votingABI,
-  provider
-) as Voting;
-
-const contractWithSigner = contract.connect(provider.getSigner());
 
 type EthContextType = {
   account: string;
   connectWallet: () => void;
   chainId: string;
   isOwner: boolean;
-  isVoter : boolean;
+  isVoter: boolean;
   contractWithSigner: Voting;
 };
 
@@ -39,13 +23,31 @@ export const EthContext = createContext<EthContextType>(null);
 
 export const EthProvider = ({ children }) => {
   const [account, setAccount] = useState("");
-  const [chainId, setChainId] = useState("")
+  const [chainId, setChainId] = useState("");
   const [isOwner, setIsOwner] = useState<boolean>(false);
   const [isVoter, setIsVoter] = useState<boolean>(false);
   const toast = useToast();
 
+  const { ethereum } = (typeof window !== "undefined" ? window : {}) as {
+    ethereum: any;
+  };
+  if (!ethereum) return null;
+  const ethereumWindow = !!window
+    ? ((window as unknown as any)
+        .ethereum as import("ethers").providers.ExternalProvider)
+    : null;
+  const provider = new ethers.providers.Web3Provider(ethereumWindow);
+
+  const contract = new ethers.Contract(
+    votingContractAddress,
+    votingABI,
+    provider
+  ) as Voting;
+
+  const contractWithSigner = contract.connect(provider.getSigner());
+
   const checkEthereumExists = () => {
-    if (!ethereum) { 
+    if (!ethereum) {
       toast({
         title: "Error",
         description: "Please Install MetaMask.".slice(0, 500) + "...",
@@ -56,35 +58,36 @@ export const EthProvider = ({ children }) => {
     return true;
   };
 
-  
-  function  getConnectedAccounts() {
-    ethereum.request({ method: "eth_accounts" })
+  function getConnectedAccounts() {
+    ethereum
+      .request({ method: "eth_accounts" })
       .then((accounts) => {
         setAccount(accounts[0]);
       })
       .catch((error) => {
-        console.log ('getConnectedAccounts - error :', error.message);
+        console.log("getConnectedAccounts - error :", error.message);
         toast({
           title: "Error",
           description: error.message.slice(0, 500) + "...",
           status: "error",
         });
       });
-  };
-  
+  }
 
   function connectWallet() {
-    if (checkEthereumExists()) {contract
-      ethereum.request({ method: "eth_requestAccounts" })
+    if (checkEthereumExists()) {
+      contract;
+      ethereum
+        .request({ method: "eth_requestAccounts" })
         .then((accounts) => {
           setAccount(accounts[0]);
-          console.log('connectWallet - account : ', accounts);
+          console.log("connectWallet - account : ", accounts);
         })
         .catch((err) => {
           if (err.code === 4001) {
             toast({
               title: "Error",
-              description: 'Please connect to Metamask'.slice(0, 500) + "...",
+              description: "Please connect to Metamask".slice(0, 500) + "...",
               status: "error",
             });
           } else {
@@ -96,32 +99,36 @@ export const EthProvider = ({ children }) => {
           }
         });
     }
-  };
-  
+  }
+
   const removeHandledContractEvents = async () => {
-    contract.removeAllListeners("VoterRegistered")
-    contract.removeAllListeners("WorkflowStatusChange")
-    contract.removeAllListeners("ProposalRegistered")
-    contract.removeAllListeners("Voted")
-  }
+    contract.removeAllListeners("VoterRegistered");
+    contract.removeAllListeners("WorkflowStatusChange");
+    contract.removeAllListeners("ProposalRegistered");
+    contract.removeAllListeners("Voted");
+  };
 
-  const handleAccountEvents = async ()=>  {
+  const handleAccountEvents = async () => {
     ethereum.on("accountsChanged", getConnectedAccounts);
-    ethereum.on('chainChanged', (chainId) => setChainId(chainId));
-  }
+    ethereum.on("chainChanged", (chainId) => setChainId(chainId));
+  };
 
-  const removeHandledAccountEvents = async ()=>  {
+  const removeHandledAccountEvents = async () => {
     ethereum.removeAllListeners("accountsChanged");
-    ethereum.removeAllListeners('chainChanged');
-
-  }
+    ethereum.removeAllListeners("chainChanged");
+  };
 
   useEffect(() => {
     if (checkEthereumExists()) {
       getConnectedAccounts();
-      ethereum.request({ method: "eth_chainId" }).then((chainId) => {setChainId(chainId)});
-      handleAccountEvents()
-      console.log('useLayoutEffect[] - {isOwner, account} : ' , {isOwner, account})
+      ethereum.request({ method: "eth_chainId" }).then((chainId) => {
+        setChainId(chainId);
+      });
+      handleAccountEvents();
+      console.log("useLayoutEffect[] - {isOwner, account} : ", {
+        isOwner,
+        account,
+      });
     }
     return () => {
       if (checkEthereumExists()) {
@@ -130,7 +137,6 @@ export const EthProvider = ({ children }) => {
       removeHandledContractEvents();
     };
   }, []);
-
 
   useEffect(() => {
     const getOwner = async () => {
@@ -141,35 +147,49 @@ export const EthProvider = ({ children }) => {
 
     const checkIsVoterRegistered = async () => {
       console.log(contractWithSigner, "contract");
-      const myVoter : Voter = await contractWithSigner.getVoter(account);
-      return(myVoter.isRegistered)
+      const myVoter: Voter = await contractWithSigner.getVoter(account);
+      return myVoter.isRegistered;
     };
 
     if (account) {
       getOwner().then((ownerAddress) => {
-        console.log('useEffect[account] - ownerAddress : ', ownerAddress )
-        if(ethers.utils.getAddress(ownerAddress) === ethers.utils.getAddress(account)){
-          setIsOwner(true)
+        console.log("useEffect[account] - ownerAddress : ", ownerAddress);
+        if (
+          ethers.utils.getAddress(ownerAddress) ===
+          ethers.utils.getAddress(account)
+        ) {
+          setIsOwner(true);
         } else {
           setIsOwner(false);
         }
-      })
+      });
 
       checkIsVoterRegistered()
         .then((isVoterRegistered) => {
-          setIsVoter(isVoterRegistered)
-          console.log('useEffect[account] - isVoterRegistered : ', isVoterRegistered )
+          setIsVoter(isVoterRegistered);
+          console.log(
+            "useEffect[account] - isVoterRegistered : ",
+            isVoterRegistered
+          );
         })
         .catch((error) => {
-          setIsVoter(false)
-          console.log('useEffect[account] - isVoterRegistered - error: ')
-        })
+          setIsVoter(false);
+          console.log("useEffect[account] - isVoterRegistered - error: ");
+        });
     }
-  }, [account])
+  }, [account]);
 
-  
   return (
-    <EthContext.Provider value={{ account, chainId, connectWallet, isOwner, isVoter, contractWithSigner}}>
+    <EthContext.Provider
+      value={{
+        account,
+        chainId,
+        connectWallet,
+        isOwner,
+        isVoter,
+        contractWithSigner,
+      }}
+    >
       {children}
     </EthContext.Provider>
   );
