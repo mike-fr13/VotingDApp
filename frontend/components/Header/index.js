8806f4f2c7dd42a7310163cf9a76c9b237760f00
Noam-m33 <noam33700@hotmail.fr>
2023-07-01 18:02:42 +0200

    feat: add title in header

--- frontend/components/Header/index.js
+++ frontend/components/Header/index.js
@@ -2,14 +2,19 @@
 import React from "react";
 import MetamaskButton from "../MetamaskButton/index";
 import AdminButton from "../AdminButton";
-import { Flex, Spacer } from "@chakra-ui/react";
+import { Flex, Spacer, Stack, Text } from "@chakra-ui/react";
 
 export const Header = () => {
   return (
-    <Flex width={"100%"}>
-      <AdminButton />
+    <Flex width={"100%"} mb={10}>
+      <Text ml={10} fontSize={28} fontWeight={"bold"}>
+        🗳️ Voting Dapp
+      </Text>
       <Spacer />
-      <MetamaskButton />
+      <Stack direction={"row"} alignItems={"center"}>
+        <AdminButton />
+        <MetamaskButton />
+      </Stack>
     </Flex>
   );
 };
